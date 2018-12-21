e3a5132545db8c791917362a7cab0f6f3031b14f
yangwx <yangwx2904@gmail.com>
2018-12-21 18:16:20 +0800

    setting file

---
+++ settings.py
@@ -0,0 +1,24 @@
+
+
+
+class Settings():
+    
+    """ 存储《外星人》的所有设置类"""
+    
+    def __init__(self):
+        """ 初始化游戏的设置 """
+        # 屏幕设置
+        
+        self.screen_with = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        
+        # 飞船的设置
+        self.ship_speed_factor = 1.5 #移动速度
+        
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3    # 限制子弹个数
